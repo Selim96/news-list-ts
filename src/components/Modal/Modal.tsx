95fc8cd3ef52a2d34331fca518f848bdcba478fa
Selim Fazylov <selim.fazylov1@gmail.com>
2023-05-13 01:26:34 +0300

    modal style

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -43,21 +43,29 @@ const Modal: React.FC = () => {
     return createPortal(
         <div className={s.overlay} onClick={closeModalByClick}>
             <div className={s.modal}>
+                <div className={s.background}></div>
                 <span className={s.closeBtn} onClick={closeModalByButton}><CloseIcon/></span>
                 <p className={s.text}>This site was create with Spaceflight News API.</p>
                 <p className={s.text}>If you have any questions contact author.</p>
                 <p className={s.text}>Contacts:</p>
                 <ul className={s.contacts}>
-                    <li className={s.contacts_item}><a href="mailto:selim.fazylov1@gmail.com">selim.fazylov1@gmail.com</a></li>
-                    <li className={s.contacts_item}><a href="tel:+380939090496">+380939090496</a></li>
+                    <li className={s.contacts_item}>
+                        <span className={s.contacts_element}>E-mail: </span>
+                        <a href="mailto:selim.fazylov1@gmail.com">selim.fazylov1@gmail.com</a>
+                    </li>
+                    <li className={s.contacts_item}>
+                        <span className={s.contacts_element}>Phone: </span>
+                        <a href="tel:+380939090496">+380939090496</a>
+                    </li>
                     <li className={s.contacts_item}>Kyiv</li>
                 </ul>
                 <p className={s.text}>Or send a message</p>
                 <form onSubmit={onSubmit} className={s.form}>
-                    <input type='email' className={s.input} />
-                    <input type='textbox' />
+                    <input type='email' name='email' className={s.input} placeholder='E-mail'/>
+                    <textarea name='message' rows={5} cols={10} placeholder="Enter your message here..."/>
                     <button type='submit' className={s.button}>Send</button>
                 </form>
+                
             </div>
         </div>,
         modalRoot
