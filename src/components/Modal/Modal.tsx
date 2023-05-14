902d7c4837a8a8bf27d4804d5c2605a10d3c0a19
Selim Fazylov <selim.fazylov1@gmail.com>
2023-05-15 00:29:16 +0300

    form editing

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect } from 'react';
+import React, {useState, useCallback, useEffect } from 'react';
 import { useAppDispatch } from '../../redux/hooks';
 import { toggleModal } from '../../redux/slice';
 import { createPortal } from "react-dom";
@@ -8,6 +8,8 @@ import s from './Modal.module.scss';
 const modalRoot = document.querySelector('#modal-root')!;
 
 const Modal: React.FC = () => {
+    const [email, setEmail] = useState("");
+    const [message, setMessage] = useState("");
 
     const dispatch = useAppDispatch();
 
@@ -28,10 +30,24 @@ const Modal: React.FC = () => {
         closeModal();
     };
 
-    const onSubmit = (e:any) => {
+    const onSubmit = (e: any) => {
         e.preventDefault();
+        console.log("email:", email)
+        console.log("message:", message)
+        // setEmail("");
+        // setMessage("");
         closeModal();
-    }
+    };
+
+    const handlInput = (e: any) => {
+        if (e.currentTarget.name === "email") {
+            setEmail(e.target.value);
+        }
+        if (e.currentTarget.name === "message") {
+            setMessage(e.target.value);
+        } 
+        
+    };
 
     useEffect(() => {
     window.addEventListener("keydown", closeModalByEsc);
@@ -41,14 +57,13 @@ const Modal: React.FC = () => {
   }, [closeModalByEsc]);
 
     return createPortal(
-        <div className={s.overlay} onClick={closeModalByClick}>
+        <div className={s.overlay} onClick={closeModalByClick} >
             <div className={s.modal}>
                 <div className={s.background}></div>
                 <span className={s.closeBtn} onClick={closeModalByButton}><CloseIcon/></span>
-                <p className={s.text}>This site was create with Spaceflight News API.</p>
                 <p className={s.text}>If you have any questions contact author.</p>
-                <p className={s.text}>Contacts:</p>
-                <ul className={s.contacts}>
+                <p className={s.contacts}>Contacts:</p>
+                <ul className={s.contacts_list}>
                     <li className={s.contacts_item}>
                         <span className={s.contacts_element}>E-mail: </span>
                         <a href="mailto:selim.fazylov1@gmail.com">selim.fazylov1@gmail.com</a>
@@ -57,15 +72,28 @@ const Modal: React.FC = () => {
                         <span className={s.contacts_element}>Phone: </span>
                         <a href="tel:+380939090496">+380939090496</a>
                     </li>
-                    <li className={s.contacts_item}>Kyiv</li>
                 </ul>
                 <p className={s.text}>Or send a message</p>
                 <form onSubmit={onSubmit} className={s.form}>
-                    <input type='email' name='email' className={s.input} placeholder='E-mail'/>
-                    <textarea name='message' rows={5} cols={10} placeholder="Enter your message here..."/>
+                    <input
+                        type='email'
+                        name='email'
+                        className={s.input}
+                        value={email}
+                        onChange={handlInput}
+                        placeholder='E-mail'
+                        required />
+                    <textarea
+                        name='message'
+                        rows={5}
+                        cols={10}
+                        className={s.textarea}
+                        value={message}
+                        onChange={handlInput}
+                        required 
+                        placeholder="Enter your message here..." />
                     <button type='submit' className={s.button}>Send</button>
                 </form>
-                
             </div>
         </div>,
         modalRoot
