56201d042005a920029dc131963b7d97048c3eb2
Selim Fazylov <selim.fazylov1@gmail.com>
2023-05-18 23:51:48 +0300

    fix upbtn

--- src/components/UpButton/UpButton.tsx
+++ src/components/UpButton/UpButton.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
+import { useAppSelector } from '../../redux/hooks';
+import allSelectors from '../../redux/selectors';
 import { useInView } from "react-intersection-observer";
 import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
 import s from './UpButton.module.scss';
-
-
 
 const UpButton:React.FC = () => {
     const { ref, inView } = useInView({
         threshold: 0
     });
+
+    const count = useAppSelector(allSelectors.getCount);
 
     const buttonStyles = [s.button];
     if (!inView) {
@@ -17,7 +19,7 @@ const UpButton:React.FC = () => {
 
     return (
         <div className={s.wrapper}>
-            <button className={buttonStyles.join(" ")} disabled={inView} type='button' >
+            <button className={buttonStyles.join(" ")} disabled={inView} type='button' style={{display: count < 4 ? "none" : "initial"}}>
                 <a href='#title'>
                     <ArrowUpwardIcon fontSize="large" />
                 </a>
