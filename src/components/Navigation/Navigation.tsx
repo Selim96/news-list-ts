837611a93c9533b1d36f6c577094f6c3d178b1db
Selim Fazylov <selim.fazylov1@gmail.com>
2023-05-20 14:06:26 +0300

    add navlink fix

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import s from './Navigation.module.scss';
 
+const setActiveLink = ({isActive}: any)=> isActive ? `${s.navigation_link} ${s.active}` : s.navigation_link;
+
 const Navigation: React.FC = () => {
     return <nav className={s.navigation}>
-        <NavLink to='/' className={({isActive, isPending})=> isPending ? s.navigation_link : isActive ? `${s.navigation_link} ${s.active}` : s.navigation_link}>Home</NavLink>
-        <NavLink to='/reports' className={({isActive, isPending})=> isPending ? s.navigation_link : isActive ? `${s.navigation_link} ${s.active}` : s.navigation_link}>Reports</NavLink>
-        <NavLink to='/blog' className={({isActive, isPending})=> isPending ? s.navigation_link : isActive ? `${s.navigation_link} ${s.active}` : s.navigation_link}>Blog</NavLink>
+        <NavLink to='/' className={setActiveLink}>Home</NavLink>
+        <NavLink to='/reports' className={setActiveLink}>Reports</NavLink>
+        <NavLink to='/blog' className={setActiveLink}>Blog</NavLink>
     </nav>
 };
 
