2ffc5b0fc257f67ac6739f73b324fca6acd3951e
Selim Fazylov <selim.fazylov1@gmail.com>
2023-05-20 13:32:41 +0300

    add navlink

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import s from './Navigation.module.scss';
 
 const Navigation: React.FC = () => {
     return <nav className={s.navigation}>
-        <Link to='/' className={s.navigation_link}>Home</Link>
-        <Link to='/reports' className={s.navigation_link}>Reports</Link>
-        <Link to='/blog' className={s.navigation_link}>Blog</Link>
+        <NavLink to='/' className={({isActive, isPending})=> isPending ? s.navigation_link : isActive ? `${s.navigation_link} ${s.active}` : s.navigation_link}>Home</NavLink>
+        <NavLink to='/reports' className={({isActive, isPending})=> isPending ? s.navigation_link : isActive ? `${s.navigation_link} ${s.active}` : s.navigation_link}>Reports</NavLink>
+        <NavLink to='/blog' className={({isActive, isPending})=> isPending ? s.navigation_link : isActive ? `${s.navigation_link} ${s.active}` : s.navigation_link}>Blog</NavLink>
     </nav>
 };
 
