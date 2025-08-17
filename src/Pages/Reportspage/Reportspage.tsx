345f5786b90b6f649d8774ebf06b6bf63e357d92
Selim96 <selim.fazylov1@gmail.com>
2025-08-17 23:23:14 +0300

    change

--- src/Pages/Reportspage/Reportspage.tsx
+++ src/Pages/Reportspage/Reportspage.tsx
@@ -21,17 +21,17 @@ const Reportspage: React.FC = () => {
     return (
         <Container>
             <div className={s.mainBox}>
-                <h1 className={s.title} id="title">Reports Page</h1> 
+                <h1 className={s.title} id="title">Reports Page</h1>
+                <ul className={s.list}> 
                 {allReports.length !== 0 && allReports.map((item: IReport) => 
-                <ul className={s.list}>
                     <li key={item.id} className={s.item}>
                         <h2 className={s.reportTitle}>{item.title}</h2>
                         <p className={s.reportSummary}>{item.summary}</p>
                         <p className={s.reportDate}>Published on: {new Date(item.published_at).toLocaleDateString()}</p>
                         <a href={item.url} target="_blank" rel="noopener noreferrer" className={s.reportLink}>Read more</a>
                     </li>
-                </ul>
                 )}
+                </ul>
             </div>
         </Container>
         
